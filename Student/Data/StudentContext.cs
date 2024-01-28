b6ab542f75860ac6acd48d77076f98cd677b4c8a
LabUser <LabUser@DESKTOP-BT4131D>
2024-01-27 23:44:16 -0500

    Add project files.

---
+++ Student/Data/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Student.Model;
+
+namespace Student.Data
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext (DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student.Model.User> User { get; set; } = default!;
+    }
+}
